030213b60a1cbe419d563cfc6dd0f1a9f7504ad1
Rahat <minhajurrohoman9016@gmail.com>
2023-11-13 14:12:32 +0600

    updated react helmet

--- src/components/common/Helmet.tsx
+++ src/components/common/Helmet.tsx
@@ -1,11 +1,16 @@
 import { Helmet } from 'react-helmet'
 interface TitleType {
-    title: string
+    title: string;
+    desc: string
 }
-export function ReactHelmet ({ title } : TitleType) {
+const ReactHelmetDefaultValue = {
+  desc: 'KM Air Aviation Ltd. is a leading name in the aviation industry, committed to providing top-notch air travel experiences for passengers around the world. As a premier airline, we pride ourselves on our dedication to safety, comfort, and convenience. Our story began with a vision to transform air travel into an unforgettable journey, and today, we continue to pursue excellence in everything we do.'
+}
+export function ReactHelmet ({ title, desc = ReactHelmetDefaultValue?.desc } : TitleType) {
   return (
       <Helmet>
         <meta charSet='utf-8' />
+        <meta name={title} content={desc} />
         <title>{title} | KM-AIR & AVIATION</title>
       </Helmet>
   )
